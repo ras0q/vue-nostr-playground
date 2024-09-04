5b65f638f251c0bb69b71b364cec9b5ec5783445
ras0q <ras0q@users.noreply.github.com>
2024-09-04 21:12:17 +0900

    :sparkles: impl nostr

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,72 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import type { WindowNostr } from 'nostr-tools/nip07'
+import { ref } from 'vue';
+import { Relay, type RelayRecord } from 'nostr-tools/relay';
+import type { Event } from 'nostr-tools';
+
+declare global {
+  interface Window {
+    nostr: WindowNostr;
+  }
+}
+
+const publicKey = ref<string>()
+const relays = ref<RelayRecord>()
+const events = ref<Event[]>([])
+const isStreaming = ref<Record<string, boolean>>({})
+
+const handleLoginButtonClick = async () => {
+  publicKey.value = await window.nostr.getPublicKey()
+  relays.value = await window.nostr.getRelays()
+
+  for (const [relayURL, { read, write }] of Object.entries(relays.value)) {
+    if (!read && !write) continue
+
+    const relay = await Relay.connect(relayURL)
+    console.info(`Connected: ${relay.url}`)
+
+    isStreaming.value[relay.url] = false
+    relay.subscribe([{
+      // https://github.com/nostr-protocol/nips#event-kinds
+      kinds: [
+        1 // Short Text Note
+      ],
+      limit: 20,
+    }], {
+      onevent: (e) => {
+        if (isStreaming.value[relay.url]) {
+          console.info(`New Event(streaming): ${e.id}`)
+          events.value.unshift(e)
+        } else {
+          console.info(`New Event(stored): ${e.id}`)
+          events.value.push(e)
+        }
+      },
+      oneose: () => {
+        console.info("Eose")
+        isStreaming.value[relay.url] = true
+      },
+      onclose: (reason) => {
+        console.info(`Closing: ${reason}`)
+      }
+    })
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <button @click="handleLoginButtonClick">Login with NIP-07</button>
+    <div>PublicKey: {{ publicKey }}</div>
+    <div>Relays: {{ relays }}</div>
+    <div>Events:</div>
+    <ul>
+      <li v-for="event in events" :key="event.id">{{
+        `${event.pubkey.substring(0, 6)} (${new
+          Date(event.created_at * 1000).toISOString().substring(11, 16)}):
+        ${event.content}` }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
